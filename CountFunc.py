03abc007f9a673a24d33158cb896fc59b960cf42
Karan Arora <mail.arora.karan@gmail.com>
2025-10-26 11:16:49 +0530

    New Commit!

---
+++ CountFunc.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "apple", "cherry", "apple"]
+print(fruits.count("apple"))   # Output: 3
+print(fruits.count("banana"))  # Output: 1
+print(fruits.count("orange"))  # Output: 0 (not in list)
+
+letters=["h", "i", "p", "o", "s", "a", "t", "r", "s", "f", "h", "i"]
+print("The letter h appears", letters.count("h"), "times in the list.")
+print("The letter s appears", letters.count("s"), "times in the list.")
---
+++ Len_List_Append.py
@@ -0,0 +1,7 @@
+fruit = ["oranges", "apples", "bananas"]
+fruit_length=len(fruit)
+print("I need to get", fruit_length, "fruits from the shop.")
+fruit.append("grapes")
+print("Oh wait, I also need to get grapes.")
+print("Now, I need to get", len(fruit), "fruits from the shop.")
+print("Ok, I will buy", fruit)
